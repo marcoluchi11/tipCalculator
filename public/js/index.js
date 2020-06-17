63de7743ebe468d03ce72429bdfd4254ef98a763
Marco Luchi <marcoaluchi@hotmail.com>
2020-06-17 18:24:08 -0300

    se agrego la func de borrarhijos creados y se agregaron las validaciones

--- public/js/index.js
+++ public/js/index.js
@@ -6,9 +6,14 @@ const seccion = document.getElementById("propina");
 let valorFinal;
 let calcularPropina = () => {
   let precio = parseInt(importe.value);
+  if (validacionDeDatos(precio)) {
+    return;
+  }
   let nota = parseInt(servicio.value);
-
   let cantidad = parseInt(cant.value);
+  if (validacionDeDatos(cantidad)) {
+    return;
+  }
   switch (nota) {
     case 1:
       valorFinal = (precio * 0.04) / cantidad;
@@ -23,7 +28,6 @@ let calcularPropina = () => {
       valorFinal = (precio * 0.1) / cantidad;
       break;
     case 5:
-      console.log("entra aca");
       valorFinal = (precio * 0.125) / cantidad;
       break;
   }
@@ -32,9 +36,32 @@ let calcularPropina = () => {
 };
 
 btn.addEventListener("click", () => {
-  let propina = document.createElement("h5");
+  borrarHijos();
+  if (calcularPropina()) {
+    let propina = document.createElement("h5");
 
-  propina.textContent =
-    "La propina es de $ " + calcularPropina() + " por persona";
-  seccion.appendChild(propina);
+    propina.textContent =
+      "La propina es de $ " + calcularPropina() + " por persona";
+    seccion.appendChild(propina);
+  }
 });
+
+function borrarHijos() {
+  var e = document.querySelector("#propina");
+  var hijo = e.lastElementChild;
+  while (hijo) {
+    e.removeChild(hijo);
+    hijo = e.lastElementChild;
+  }
+}
+
+let validacionDeDatos = (dato) => {
+  if (isNaN(dato) || dato < 0 || dato === 0) {
+    alert("Ingresaste un dato incorrecto, ingresalo de manera correcta");
+    importe.value = "";
+    cant.value = "";
+    return true;
+  } else {
+    return false;
+  }
+};
